e1ddff9f0a0f76534d0b105844cf185b852066fe
Steven Nunez <steven.nunez@gmail.com>
2016-02-12 11:01:08 -0500

    Find All Drinks

--- app/routes/drinks.js
+++ app/routes/drinks.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(){
-    return ["Jersey Turnpike", "Rum and Coke"];
+    return this.store.findAll('drink');
   },
   actions:{
     addDrink(drinkToBeAdded){
